c12655c7badbd4d8c89eeda73c5852baa9035825
Rodion <57228093+LuminoDiode@users.noreply.github.com>
2023-09-30 23:31:29 +0300

    added endpoint

--- vdb_main_server_api/Controllers/LinksController.cs
+++ vdb_main_server_api/Controllers/LinksController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using ServicesLayer.Models.Common;
+using ServicesLayer.Services;
+using System.Configuration;
 
 namespace main_server_api.Controllers;
 
@@ -10,4 +13,17 @@ namespace main_server_api.Controllers;
 [Produces("application/json")]
 public class LinksController : ControllerBase
 {
+	private readonly LinksInfo _links;
+
+	public LinksController(SettingsProviderService sp)
+	{
+		this._links = sp.LinksInfo;
+	}
+
+	[HttpGet]
+	[Route("latest-release")]
+	public async Task<IActionResult> GetLatestReleaseLink()
+	{
+		return await Task.FromResult(Ok(_links.LatestReleaseLink));
+	}
 }
